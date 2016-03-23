c4f89a850672d0e4206051008854bad4b890e214
Kim Sung Hwan <3dartmax@naver.com>
2016-03-23 11:54:22 +0900

    css에 input적용 테스트

--- testmodelform/forms.py
+++ testmodelform/forms.py
@@ -1,6 +1,19 @@
 from django import forms
 from .views import Author, Book
-from .models import TITLE_CHOICES
+
+# python manage.py shell에서 테스트법.
+# >>> from django import forms
+# >>> from testmodelform.views import Author, Book
+# >>> from testmodelform.forms import DefaultTextInput
+# >>> w = DefaultTextInput()
+# >>> print(w)
+# >>> print(w.media)
+# >>> print(w.media['css'])
+class DefaultTextInput(forms.TextInput):
+    class Media:
+        css = {
+            'all': ('css/default.css')
+        }
 
 # 방법1.
 class AuthorForm(forms.ModelForm):
@@ -10,7 +23,15 @@ class AuthorForm(forms.ModelForm):
         localized_fields = ('name', 'birth_date',)           # 보여줄폼에 히스토리 리스트.
         #exclude         = ['title']                            # fields에서 제외시킴.
         widgets = {
-           'name': forms.Textarea(attrs={'rows': 10, 'cols': 80 }),
+            #'name': DefaultTextInput(),
+            #'name': forms.Textarea(attrs={'rows': 10, 'cols': 80 }),
+            #'name': forms.TextInput(attrs={'style': 'border: 1px solid #ff0000;'}),
+            #'name': forms.TextInput(attrs={'style': 'background-color: #e2e2e2;'}),
+            #'birth_date': forms.TextInput(attrs={'readonly': 'readonly'}),      # 읽기전용 처리.
+            #'birth_date': forms.TextInput(attrs={'disabled': 'disabled'}),      # 비활성화 처리.
+
+            # 'css/default.css'정의한 사용자 class="default"적용.
+            'name': forms.TextInput(attrs={'class': 'default'}),
         }
         labels = {
             'name': '이름',
@@ -31,7 +52,8 @@ class BookForm(forms.ModelForm):
         localized_fields = ('name',)                         # 보여줄폼에 히스토리 리스트.
         widgets = {
             #'name': forms.NumberInput(attrs={'class': 'input-mini'}),
-            'name': forms.TextInput(attrs={'class': 'input-mini'}),
+            #'name': forms.TextInput(attrs={'class': 'input-mini'}),
+            'name': forms.TextInput(attrs={'style': 'background-color: #e2e2e2;'}),
         }
         labels = {
             'name': '책이름',
@@ -46,6 +68,8 @@ class BookForm(forms.ModelForm):
         }
 
 """
+from .models import TITLE_CHOICES
+
 # 방법2.
 class AuthorForm(forms.Form):
     name       = forms.CharField(max_length=100)
